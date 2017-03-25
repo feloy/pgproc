26afca9d910846056bcf6200e4a66fee6348c1cd
pmartin <contact@elol.fr>
2017-03-25 09:39:13 +0100

    first tests

--- pgproc.go
+++ pgproc.go
@@ -2,12 +2,15 @@ package pgproc
 
 import (
 	"database/sql"
+	"fmt"
+	"github.com/lib/pq"
 )
 
 type PgProc struct {
 	db *sql.DB
 }
 
+// NewPgProc creates a new connection to a PostgreSQL database
 func NewPgProc(conninfo string) (*PgProc, error) {
 	var pgproc = PgProc{}
 	var err error
@@ -16,4 +19,33 @@ func NewPgProc(conninfo string) (*PgProc, error) {
 		return nil, err
 	}
 	return &pgproc, nil
+}
+
+// Call calls a PostgreSQL procedure and stores the result
+func (p *PgProc) Call(result interface{}, schema string, proc string, params ...interface{}) error {
+
+	query := fmt.Sprintf("SELECT * FROM %s.%s(%s)",
+		pq.QuoteIdentifier(schema),
+		pq.QuoteIdentifier(proc),
+		paramsString(len(params)))
+
+	row := p.db.QueryRow(query, params...)
+	err := row.Scan(result)
+	return err
+}
+
+//
+// Local static functions
+//
+
+// paramsString returns a string $1,$2,...,$len
+func paramsString(len int) string {
+	if len == 0 {
+		return ""
+	}
+	result := "$1"
+	for i := 2; i <= len; i++ {
+		result += fmt.Sprintf(",$%d", i)
+	}
+	return result
 }
--- pgproc_test.go
+++ pgproc_test.go
@@ -1,13 +1,74 @@
 package pgproc
 
 import (
-	"testing"
+	"fmt"
 	_ "github.com/lib/pq"
+	"testing"
+)
+
+var (
+	user     = "pgproctest"
+	password = "pgproctest"
+	host     = "localhost"
+	dbname   = "pgproctest"
 )
 
 func TestNewPgProc(t *testing.T) {
-	_, err := NewPgProc("user=pgproctest password=pgproctest host=localhost dbname=pgproctest sslmode=disabled")
+	conninfo := fmt.Sprintf("user=%s password=%s host=%s dbname=%s sslmode=disable",
+		user, password, host, dbname)
+	_, err := NewPgProc(conninfo)
 	if err != nil {
 		t.Errorf("Error connecting to db")
+	}
+}
+
+func connect() (*PgProc, error) {
+	conninfo := fmt.Sprintf("user=%s password=%s host=%s dbname=%s sslmode=disable",
+		user, password, host, dbname)
+	base, err := NewPgProc(conninfo)
+	return base, err
+}
+
+func TestParamsString(t *testing.T) {
+	args0 := paramsString(0)
+	wanted0 := ""
+	if args0 != wanted0 {
+		t.Errorf("paramsString with 0 params should be '%s' but is '%s'",
+			wanted0, args0)
+	}
+
+	args1 := paramsString(1)
+	wanted1 := "$1"
+	if args1 != wanted1 {
+		t.Errorf("paramsString with 1 params should be '%s' but is '%s'",
+			wanted1, args1)
+	}
+
+	args4 := paramsString(4)
+	wanted4 := "$1,$2,$3,$4"
+	if args4 != wanted4 {
+		t.Errorf("paramsString with 4 params should be '%s' but is '%s'",
+			wanted4, args4)
+	}
+}
+
+func TestCallUnknown(t *testing.T) {
+	base, err := connect()
+	var res int
+	err = base.Call(&res, "tests", "unknown_function")
+	if err == nil {
+		t.Errorf("Error calling unknown function")
+	}
+}
+
+func TestCallReturnsInteger(t *testing.T) {
+	base, err := connect()
+	var res int
+	err = base.Call(&res, "tests", "test_returns_integer")
+	if err != nil {
+		t.Errorf("Error calling tests.test_returns_integer")
+	}
+	if res != 42 {
+		t.Errorf("Error expected %d value is %d", 42, res)
 	}
 }
